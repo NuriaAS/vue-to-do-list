8a1b311f2303c93050cbfad07cc097087ab6e04f
nuriaandresfoto <nuriaandresfoto@gmail.com>
2021-04-21 19:29:46 +0200

    Saves input data in the component
    Saves the input data in the model.
    
    Puts this data in and object and keep it in an array.
    
    Resset the data.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="header">
     <h1>{{ msg }}</h1>
-    <form class="header__form">
-      
+    <form class="header__form" v-on:submit.prevent="onSubmit">
       <label for="task" class="header__form__label-task">
           Task
-          <input type="text" placeholder="Write a task" name="task" class="header__form__label-task__input">
+          <input type="text" placeholder="Write a task" name="task" class="header__form__label-task__input" v-model="newTask" required>
       </label>
       <div class="header__form__selectors">
         <label for="priority" class="header__form__selectors__label">
           Priority
-          <select name="priority" id="" class="header__form__selectors__label__select">
-              <option value="higth" selected>Higth</option>
+          <select name="priority" id="" class="header__form__selectors__label__select" v-model="newPriority" required>
+              <option value="high" selected>High</option>
               <option value="medium">Medium</option>
               <option value="low">Low</option>
           </select>
         </label>
         <label for="time" class="header__form__selectors__label">
           When
-          <input type="date" name="time" id=""  class="time">
+          <input type="date" name="time" id=""  class="time" v-model="newDate" required>
         </label>
-        <button class="header__form__selectors__submit-btn">
+        <button class="header__form__selectors__submit-btn" @click="submitHandler">
           Save
         </button>
       </div>
@@ -29,8 +28,8 @@
         <label for="" class="header__order__label">
           Order list by:
           <select name="order" id="" class="header__order__label__list-order">
-              <option value="priority" selected>Higth to low priority</option>
-              <option value="priorityReverse">Low to higth priority</option>
+              <option value="priority" selected>High to low priority</option>
+              <option value="priorityReverse">Low to high priority</option>
               <option value="created">Last to first added</option>
               <option value="createdReverse">First to last added</option>
               <option value="when">First to last to do</option>
@@ -39,9 +38,6 @@
               <option value="alphabeticalReverse">From Z to A</option>
           </select>
       </label>
-      
-      <pre>{{ $data }}</pre>
-      
     </section>
   </div>
 </template>
@@ -51,14 +47,39 @@ export default {
   name: 'ToDoList',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      newTask: "",
+      newPriority: "high",
+      newDate: "",
+      itemList: []
+    }
+  },
+  methods: {
+    setListItem() {
+      this.itemList = {
+        task: this.newTask,
+        priority: this.newPriority,
+        date: this.newDate
+      } 
+    },
+    resetModel() {
+      this.newTask = "";
+      this.newPriority = "high";
+      this.newDate = "";
+    },
+    submitHandler() {
+      if(this.newTask && this.newDate) {
+        this.setListItem();
+      }
+      this.resetModel();
+    }
   }
-  
 }
 </script>
-  
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-  
   .header {
     padding-bottom: 2rem;
   }
@@ -69,7 +90,9 @@ export default {
   .header__form input  {
     display: block;
   }
-  .header__form__label-task__input, label .header__form__selectors__label__select, label .time {
+  .header__form__label-task__input,
+  label .header__form__selectors__label__select, 
+  label .time {
     width: 100%;
   }
   .header__form__label-task__input {
@@ -80,13 +103,11 @@ export default {
     font-weight: 600;
     text-align: left;
   }
-  
   .header__form__label-task {
     display: block;
     width: 100%;
     text-align: left;
   }
- 
   .header__form__selectors__submit-btn {
     border: none;
     background-color: rosybrown;
@@ -103,7 +124,8 @@ export default {
     background-color: transparent;
     color: rosybrown;
   }
-  input, select {
+  input, 
+  select {
     border-radius: 5px;
     border: 1px solid #997676;
     height: 1.5rem;
@@ -114,15 +136,14 @@ export default {
     justify-content: space-evenly;
     align-items: flex-end;
   }
- 
   .header__order {
     border-radius: 0 0 10px 10px;
   }
-  
   @media only screen and (min-width: 767px) {
-     .header__form, .header__order {
-        width: 60%;
-        margin: auto;
-      }
+    .header__form, 
+    .header__order {
+      width: 60%;
+      margin: auto;
+    }
   }
 </style>
